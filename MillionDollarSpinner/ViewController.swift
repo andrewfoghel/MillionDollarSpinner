1215c06267e77f3325be811f152d720e398a56a8
Andrew Foghel <andrewfoghel@Andrews-MacBook-Pro.local>
2018-12-01 00:17:52 -0500

    Initial Commit

---
+++ MillionDollarSpinner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MillionDollarSpinner
+//
+//  Created by Andrew  Foghel on 12/1/18.
+//  Copyright © 2018 Andrew  Foghel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
